0bb86e0b22c11442ad6a2fd4a0d5fa1049c713cd
Marwan <emef4z@gmail.com>
2020-06-11 20:06:15 +0400

    Add save new movie feature by pressing back button

--- app/src/main/java/com/mf4z/moviekeeper/MainActivity.java
+++ app/src/main/java/com/mf4z/moviekeeper/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mTextMovieTitle;
     private EditText mTextMovieDesc;
     private Spinner mSpinnerGenre;
+    private int mMoviePosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         List<GenreInfo> genre = DataManager.getInstance().getGenre();
 
         //Array Adapter to handle populating the spinner
-        ArrayAdapter<GenreInfo> adapterGenre = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,genre);
-        adapterGenre.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item ); //Set  Layout for dropdown
+        ArrayAdapter<GenreInfo> adapterGenre = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, genre);
+        adapterGenre.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); //Set  Layout for dropdown
         mSpinnerGenre.setAdapter(adapterGenre); //Setting adapter to spinner
 
         //Read info passed between the activity via intent extra
@@ -46,9 +47,9 @@ public class MainActivity extends AppCompatActivity {
         mTextMovieTitle = (EditText) findViewById(R.id.editText_movie);
         mTextMovieDesc = (EditText) findViewById(R.id.editText_description);
 
-        if(!mIsNewMovie){   //Display if note is existing note
-        //Method to display Movies
-        displayMovies(mSpinnerGenre, mTextMovieTitle, mTextMovieDesc);
+        if (!mIsNewMovie) {   //Display if movie is existing note
+            //Method to display Movies
+            displayMovies(mSpinnerGenre, mTextMovieTitle, mTextMovieDesc);
         }
     }
 
@@ -73,16 +74,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Saves the movies when back is pressed and onPause is called
+        saveMovies();
+
+    }
+
+    private void saveMovies() {
+        mMovie.setGenre((GenreInfo)mSpinnerGenre.getSelectedItem());
+        mMovie.setTitle(mTextMovieTitle.getText().toString());
+        mMovie.setText(mTextMovieDesc.getText().toString());
+    }
+
     private void sendEmail() {
 
         String genre = mSpinnerGenre.getSelectedItem().toString();
         String subject = mTextMovieTitle.getText().toString();
-        String text = "Check out what I'm watching, " +genre+ " : \""+subject+"\" \n"+ mTextMovieDesc.getText().toString();
+        String text = "Check out what I'm watching, " + genre + " : \"" + subject + "\" \n" + mTextMovieDesc.getText().toString();
 
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("message/rfc2822"); //mime type for email
-        intent.putExtra(Intent.EXTRA_SUBJECT,subject); //set the subject of the mail
-        intent.putExtra(Intent.EXTRA_TEXT,text); //text in the email
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject); //set the subject of the mail
+        intent.putExtra(Intent.EXTRA_TEXT, text); //text in the email
         startActivity(intent);
 
     }
@@ -95,22 +111,30 @@ public class MainActivity extends AppCompatActivity {
 
         spinnerGenre.setSelection(genreIndex);
         textMovieTitle.setText(mMovie.getTitle());
-         textMovieDesc.setText(mMovie.getText() );
+        textMovieDesc.setText(mMovie.getText());
     }
 
     private void readDisplayStateValues() {
 
         Intent intent = getIntent(); //Reference to the intent
         //Get the data passed via the intent
-        int position  = intent .getIntExtra(MOVIE_POSITION, POSITION_NOT_SET);
+        int position = intent.getIntExtra(MOVIE_POSITION, POSITION_NOT_SET);
 
         //Check if it's a new note or not
         mIsNewMovie = position == POSITION_NOT_SET;
-        if (!mIsNewMovie){
-
+        if (mIsNewMovie) { //If it's new movie, create a movie
+            //Create Movie
+            createMovie();
+        }
+        else {
             //Get Movie passed by it's position
             mMovie = DataManager.getInstance().getMovies().get(position);
-
         }
+    }
+
+    private void createMovie() {
+        DataManager dm = DataManager.getInstance();
+        mMoviePosition = dm.createNewNote();
+        mMovie = dm.getMovies().get(mMoviePosition);
     }
 }
