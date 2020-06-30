c1989c79fea3b21bbf9aab775c3247c68eb33dca
Marwan <emef4z@gmail.com>
2020-06-30 17:17:44 +0400

    Implement basic test interaction for creating note

--- app/src/androidTest/java/com/mf4z/moviekeeper/MovieCreationTest.java
+++ app/src/androidTest/java/com/mf4z/moviekeeper/MovieCreationTest.java
@@ -1,5 +1,6 @@
 package com.mf4z.moviekeeper;
 
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -10,6 +11,12 @@ import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+
 @RunWith(AndroidJUnit4.class)
 public class MovieCreationTest {
 
@@ -18,5 +25,14 @@ public class MovieCreationTest {
             new ActivityTestRule<>(MovieListActivity.class);
 
     @Test
-    public void createNewMovie(){}
+    public void createNewMovie(){
+ /*       ViewInteraction fabNewMovie = onView(withId(R.id.fab));
+        fabNewMovie.perform(click());*/
+
+ //Prefered way of doing the above
+        onView(withId(R.id.fab)).perform(click()); //Locating the view and peforming and action on it
+        onView(withId(R.id.editText_movie)).perform(typeText("Test movie title"));
+        onView(withId(R.id.editText_description)).perform(typeText("This is a test movie description, should be fun!"),
+                closeSoftKeyboard());
+    }
 }
