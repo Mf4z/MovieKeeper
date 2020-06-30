b7e0541720398231e3fdf3f514b659b4edf30a82
Marwan <emef4z@gmail.com>
2020-06-30 18:04:49 +0400

    Implement spinner selection and back pressed interaction test

--- app/src/androidTest/java/com/mf4z/moviekeeper/MovieCreationTest.java
+++ app/src/androidTest/java/com/mf4z/moviekeeper/MovieCreationTest.java
@@ -4,6 +4,7 @@ import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,10 +16,19 @@ import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static androidx.test.espresso.Espresso.pressBack;
 
 
 @RunWith(AndroidJUnit4.class)
 public class MovieCreationTest {
+
+    static DataManager sDataManager;
+
+    @BeforeClass
+    public static void classSetUp(){
+        sDataManager = DataManager.getInstance();
+    }
 
     @Rule
     public ActivityTestRule<MovieListActivity> mMovieListActivityRule =
@@ -26,13 +36,30 @@ public class MovieCreationTest {
 
     @Test
     public void createNewMovie(){
+
+         final GenreInfo genre = sDataManager.getGenre("java_lang");
+        final String  movieTitle = "Test movie title";
+        final String  movieText = "This is a test movie description, should be fun!";
+
  /*       ViewInteraction fabNewMovie = onView(withId(R.id.fab));
         fabNewMovie.perform(click());*/
 
- //Prefered way of doing the above
+
+ //NB: Steps have to be sequnetial as would be when user interacts with activities
+
+        //Prefered way of doing the above
         onView(withId(R.id.fab)).perform(click()); //Locating the view and peforming and action on it
-        onView(withId(R.id.editText_movie)).perform(typeText("Test movie title"));
-        onView(withId(R.id.editText_description)).perform(typeText("This is a test movie description, should be fun!"),
+
+        //Select item from spinner, spinner particular action,not needed for listviews
+        onView(withId(R.id.spinner_genre)).perform(click());
+
+        //Perform click on spinner selcetion
+        onData(allOf(instanceOf(GenreInfo.class),equalTo(genre))).perform(click());
+
+        onView(withId(R.id.editText_movie)).perform(typeText(movieTitle));
+        onView(withId(R.id.editText_description)).perform(typeText(movieText),
                 closeSoftKeyboard());
+
+        pressBack(); //to go back to the previous activity
     }
 }
